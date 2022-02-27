cd6b16c461a838b45deb958096b633423b88e82c
Anastasia Losikova <anast.losikova@gmail.com>
2022-02-27 21:36:10 +0300

    Add WKWeb

---
+++ VK_noStoryboard/WKWebViewController.swift
@@ -0,0 +1,54 @@
+//
+//  WKWebViewController.swift
+//  VK_noStoryboard
+//
+//  Created by Анастасия Лосикова on 27.02.2022.
+//
+
+import UIKit
+import Alamofire
+import WebKit
+
+class WKWebViewController: UIViewController {
+    
+    let webView = WKWebView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupWeb()
+    }
+    
+    private func setupUI() {
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        webView.clipsToBounds = true
+    }
+    
+    private func setupWeb() {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "8089974"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "262150"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.68")
+        ]
+        
+        let request = URLRequest(url: urlComponents.url!)
+        
+        webView.load(request)
+    }
+    
+}
